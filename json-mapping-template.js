a0ecec0af8a4873e85f2f2665a66e2a570c0d21d
frankiezafe <frankiezafe@dying-mac.(none)>
2014-07-01 23:29:59 +0200

    mapping file: list > local

--- json-mapping-template.js
+++ json-mapping-template.js
@@ -5,7 +5,7 @@
 	"desc": "template of mapping file in json, july 2014",
 	"name": "mapping-template", // free text
 	"model": "bvh_numediart", // ULTRA important for mapping!!! each file with same model will have the same bones names & hierarchy
-	"list": [ "hips", "spine", "chest", "neck", "shoulderL" ], // list of bones, MUST be identical to the mocap file!
+	"local": [ "hips", "spine", "chest", "neck", "shoulderL" ], // list of bones, MUST be identical to the mocap file!
 	"foreign": [ "b_Hips", "b_Spine01", "b_Spine02", "b_Neck", "b_shoulder.l" ], // list of bones of the avatar to animate
 
 }
