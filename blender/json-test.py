4ea8298257296dbb2412a5d2910574f79f448059
frankiezafe <frankiezafe@dying-mac.(none)>
2014-07-06 10:10:30 +0200

    references

--- blender/json-test.py
+++ blender/json-test.py
@@ -1,3 +1,7 @@
+# https://docs.python.org/2/library/json.html
+# http://stackoverflow.com/questions/15525837/which-is-the-best-way-to-compress-json-to-store-in-a-memory-based-store-like-red
+# http://bsonspec.org/
+
 import json
 
 mocap = open( 'test.json', 'r' ).read()
